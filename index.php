8ccec9ad96a991416907c48db01a12eaaf39be4d
Maerryham <lawalmariamyetunde@gmail.com>
2018-04-20 17:46:45 +0100

    Add existing file

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>My Figma Design</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <div style="width: 100%;  text-align: center; height: 1000px;  padding: 40px 20px; font-weight: 18px; color:white; background-color: blue">
+		<h1 style="font-family: 'Monotype Corsiva'; font-size: 48px ; text-align: center">Hello</h1>
+		
+		<p style="font-family: arial; font-size: 64px; text-align: center"> <?= date ("H:i:a")?></p>
+		</div>
+		
+    </body>
+</html>
